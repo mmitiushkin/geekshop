0281f281d9a619c32ba70d415c9eaecacbf0b2b8
mmitiushkin <mmitiushkin@gmail.com>
2021-10-20 18:33:24 +0300

    lesson4 added app users, model user, login, register updated urls

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -18,14 +18,14 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from products.views import index, products
+from products.views import index
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', include('products.urls', namespace='products')),
-
+    path('users/', include('users.urls', namespace='users')),
 ]
 
 if settings.DEBUG:
---
+++ users/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    image = models.ImageField(upload_to='users_images', blank=True, null=True)
+
+
